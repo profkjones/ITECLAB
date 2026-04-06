907b1c3dfd3e93a405ad2f20a711af972d222617
profkjones <kyle.jones4990@sinclair.edu>
2026-04-06 16:33:25 -0400

    New SQL project

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.ITECLAB_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://eoyzsiaunmhjqasagjst.supabase.co",
+  supabaseAnonKey: "sb_publishable_aFIe33DVSpyp-wVICCSHEA_zrk0SLqe",
   workspace: "iteclab-main",
   table: "lab_tracker_state",
   syncIntervalMs: 20000,
